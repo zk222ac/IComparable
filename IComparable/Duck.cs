4638f9e80de7981adb9bdac82bb1d6ff2ab3e0f5
Zuhair Haroon Khan <zk222ac@gmail.com>
2019-02-05 09:51:38 +0100

    IComparable List Sort

---
+++ IComparable/Duck.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IComparable
+{
+   public class Duck : IComparable<Duck>
+   {
+       private int _size;
+       private KindOfDuck _kind;
+
+       public int Size { get; set; }
+       public KindOfDuck Kind { get; set; }
+
+       public int CompareTo(Duck other)
+       {
+           // one duck size is bigger than that other one
+           // its return 1
+           if (this.Size > other.Size)
+           {
+               return 1;
+           }
+           else if (this.Size < other.Size)
+           {
+               // one duck size is smaller than that other one
+               // its return -1
+               return -1;
+           }
+           else
+           {
+               return 0;    
+           }
+        }
+   }
+    public enum KindOfDuck
+    {
+        Mallard,
+        Muscovy,
+        Decoy
+    }
+}
--- IComparable/Program.cs
+++ IComparable/Program.cs
@@ -10,12 +10,34 @@ namespace IComparable
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            List<Duck> ducks = new List<Duck>()
+            {
+                new Duck(){Kind = KindOfDuck.Mallard , Size = 23},
+                new Duck(){Kind = KindOfDuck.Decoy , Size = 45},
+                new Duck(){Kind = KindOfDuck.Muscovy , Size = 12},
+                new Duck(){Kind = KindOfDuck.Decoy , Size = 56},
+                new Duck(){Kind = KindOfDuck.Mallard , Size = 22},
+                new Duck(){Kind = KindOfDuck.Muscovy , Size = 55}
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            };
+
+            foreach (var duck in ducks)
+            {
+                Console.WriteLine(duck.Kind);
+                Console.WriteLine(duck.Size);
+                Console.WriteLine("\n");
+
+            }
+            ducks.Sort();
+            Console.WriteLine(".........................");
+            foreach (var duck in ducks)
+            {
+                Console.WriteLine(duck.Kind);
+                Console.WriteLine(duck.Size);
+                Console.WriteLine("\n");
+
+            }
+            Console.ReadKey();
         }
     }
 }
